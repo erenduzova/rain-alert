02373061205e05e38a2040addc52343f62d115a2
Eren Düzova <erenduzova@gmail.com>
2022-09-17 00:30:43 +0300

    Check Next 12 Hours
    Check hourly data for weathers which requires umbrella.

--- main.py
+++ main.py
@@ -13,5 +13,12 @@ OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
 response = requests.get(url=OWM_ENDPOINT, params=PARAMETERS)
 response.raise_for_status()
 weather_data = response.json()
+next_12_data = weather_data["hourly"][:12]
 
-print(weather_data)
+will_rain = False
+for hour_d in next_12_data:
+    if int(hour_d["weather"][0]["id"]) < 700:
+        will_rain = True
+
+if will_rain:
+    print("It's going to rain today. Remember to bring an ☂️")
